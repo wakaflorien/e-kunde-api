a9670cb2dbe562b6475563a8d1db8faa6fe68e16
Florien Niyongabo <waka.florien45@gmail.com>
2025-11-03 12:55:28 +0200

    feat: configure application bootstrap and enhance logging in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log(
+      'Application is running on port 🚀🚀',
+      process.env.PORT ?? 3000,
+    );
+  })
+  .catch((err) => {
+    console.error('Error starting application:', err);
+  });
